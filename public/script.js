96c3fa19ce56694dbaaca58688eba6e160680576
Edson Jordan <edson_4555@hotmail.com>
2021-08-09 19:48:24 -0500

    adding status activate to links from navigation

--- public/script.js
+++ public/script.js
@@ -1,12 +1,27 @@
-window.addEventListener('scroll', (e)=> {
-  console.log("scroll");
-})
-
 const floatButton = document.getElementById('float-button'),
 circularMenu = document.getElementById('circular-menu');
-
 floatButton?.addEventListener('click', () => {
   floatButton.classList.toggle('pulsed');
   circularMenu.classList.toggle('expand');
 });
-
+document.getElementById('listNav').addEventListener('click', (e)=>{
+const list =  document.getElementById('listNav');
+switch (e.target.nodeName) {
+  case 'A':                       
+      list.getElementsByClassName('active')[0].classList.remove('active');
+      e.target.classList.add('active');  
+    break;
+  case 'SPAN':           
+      list.getElementsByClassName('active')[0].classList.remove('active');
+      e.target.parentNode.classList.add('active'); 
+    break;
+  case 'svg':            
+      list.getElementsByClassName('active')[0].classList.remove('active');
+      e.target.parentNode.classList.add('active'); 
+  break;
+  case 'path':            
+      list.getElementsByClassName('active')[0].classList.remove('active');
+      e.target.parentNode.parentNode.classList.add('active'); 
+  break;
+}
+});
